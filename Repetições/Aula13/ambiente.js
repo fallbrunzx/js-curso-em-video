b11ca7904230d22a64905cd2b9e025d2b77d3bf7
fallbrunzx <103600239+fallbrunzx@users.noreply.github.com>
2023-10-22 13:51:39 -0300

    Repetições

---
+++ Repetições/Aula13/ambiente.js
@@ -0,0 +1,22 @@
+    //TESTANDO LOGICO E DEPOIS EXECUTA O BLOCO
+    /*
+    while(c <= 6) {
+        console.log(`Passo ${c}`)
+        c++
+    }
+    */
+   
+   //EXECUTA O BLOCO E DEPOIS VERIFICA
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6)
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
